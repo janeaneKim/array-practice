d46fec48466a9ab898c8edd8ab5bf3a683c1f27a
janeaneKim <93021938+janeaneKim@users.noreply.github.com>
2022-07-21 00:26:24 -0700

    changed code tabs

--- src/DataStructures.java
+++ src/DataStructures.java
@@ -16,28 +16,28 @@ public class DataStructures {
 		//========== end of #1 =================
 		
 		//2. output of this code?
-		
-        double[ ] exampleArray = {1,5,6,5,4,1};
-        double maximum = exampleArray[0];
-        int index = 0;
-        
-        for (int i = 1; i<exampleArray.length; i++){
-             if (exampleArray[ i ] > maximum) {
-                  maximum = exampleArray[ i ];
-                  index = i;
-             }    
-        };
-        System.out.println("The index for problem 2 is " + index);
-        /* The output of this code is 2 (index = 2). 
-         * 
-         * 1st iteration of the for loop: i = 1, exArray[1]= 5, maximum= 1
-         * 		since 5>1, the for loop will run leaving: maximum= 5, index= 1
-         *  2nd iteration of the for loop: i = 2, exArray[2]= 6, maximum = 5
-         *  	since 6>5, the for loop will run, leaving: maximum= 6, index=2
-         *  3rd iteration of the for loop: i= 3, exArray[3]= 5, maximum= 6
-         *  	since 5>6 is NOT true, the loop will not run.
-         *  	since the loop did not run, i++ will not run and index will not be overwritten. 
-         *  	--> index remains at 2 from the 2nd loop iteration */
+
+		double[ ] exampleArray = {1,5,6,5,4,1};
+		double maximum = exampleArray[0];
+		int index = 0;
+
+		for (int i = 1; i<exampleArray.length; i++){
+		     if (exampleArray[ i ] > maximum) {
+			  maximum = exampleArray[ i ];
+			  index = i;
+		     }    
+		};
+		System.out.println("The index for problem 2 is " + index);
+		/* The output of this code is 2 (index = 2). 
+		 * 
+		 * 1st iteration of the for loop: i = 1, exArray[1]= 5, maximum= 1
+		 * 		since 5>1, the for loop will run leaving: maximum= 5, index= 1
+		 *  2nd iteration of the for loop: i = 2, exArray[2]= 6, maximum = 5
+		 *  	since 6>5, the for loop will run, leaving: maximum= 6, index=2
+		 *  3rd iteration of the for loop: i= 3, exArray[3]= 5, maximum= 6
+		 *  	since 5>6 is NOT true, the loop will not run.
+		 *  	since the loop did not run, i++ will not run and index will not be overwritten. 
+		 *  	--> index remains at 2 from the 2nd loop iteration */
 	}
 };
 
